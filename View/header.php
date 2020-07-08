83e01390cd58f297452e85cbaab4a9db343772b6
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-07-08 16:21:06 +0200

    creation du fichier header

---
+++ View/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Blog</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="row">
+                <nav class="col navbar navbar-expand navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">Jean Forteroche</a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="View/chapterView.php" class="nav-link">Chapitres</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>    
+        </div>
+    </header>
+   
+
