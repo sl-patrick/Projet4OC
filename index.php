3124e67d5543411acea928cd80e3fe3e8ff48ec3
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-07-15 16:28:18 +0200

    Classe router avec les vérifications des url pour appeler le bon contrôleur

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
 <!-- ROUTEUR -->
 <?php
-require ('Controller/homeController.php');
+
+require('Controller/Controller.php');
+
+class Router {
+
+    public function run() {
+        try {
+            if (isset($_GET['url'])) {
+                var_dump($_GET['url']);
+                if ($_GET['url'] === 'post') {
+                    if (isset($_GET['postId']) && is_int($_GET['postId'])) {
+                        //Afficher la page post.
+                        $frontController = new FrontController();
+                        $frontController->post($_GET['postId']);
+                    } else {
+                        $frontController = new FrontController();
+                        $frontController->chapters();
+                    }
+                } elseif ($_GET['url'] === 'chapter') {
+                    //Afficher la page chapitre.
+                    $frontController = new FrontController();
+                    $frontController->chapters();
+                } else {
+                    //Afficher page d'erreur.
+                    echo'page inconnue';
+                }
+            } else {
+                //Afficher la page d'accueil.
+                $frontController = new FrontController();
+                $frontController->home();
+            }
+        } catch (Exception $error) {
+            echo $error->getMessage();
+        }
+    }  
+}
+
+$router = new Router();
+$router->run();
