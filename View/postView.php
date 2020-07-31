62974ad830a85624ac6abac6bf4dc2a0b5f63f80
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-07-31 11:49:47 +0200

    séparation de la vue pour afficher et ajouter un commentaire dans deux script distinct.

--- View/postView.php
+++ View/postView.php
@@ -26,40 +26,18 @@ $post = $posts->fetch();
     </div>
 
     <?php
+    
     $posts->closeCursor();
+    
     ?>
-
     <!-- Afficher commentaires -->
-    <?php
-    while ($comment = $comments->fetch()) {
-    ?>
-    <div>
-        <h2>Commentaires</h2>
-        <p><?= htmlspecialchars($comment['pseudo']); ?></p>
-        <p><?= htmlspecialchars($comment['contents']); ?></p>
-        <p><?= htmlspecialchars($comment['creation_date']); ?></p>
-    </div>
-    <?php
-    }
-    $comments->closeCursor();
-    ?>
-
+    <?php require 'displayCommentsView.php'; ?>
     <!-- Ajouter commentaires -->
-    <div>
-        <h3>Ajouter un commentaire</h3>
-        <form action="index.php?url=postComment&amp;postId=<?= htmlspecialchars($_GET['postId']); ?>" method="post">
-            <div class="form-group">
-                <label for="author">Pseudo : </label>
-                <input type="text" name="author" id="author">
-            </div>
-            <div class="form-group">
-                <label for="contents">Message : </label>
-                <textarea name="contents" id="contents" class="form-control"></textarea>
-            </div>
-            <input type="submit" value="Envoyer">
-        </form>
-    </div>
-   
+    <?php require 'commentsFormView.php'; ?>
+
+
+
+
 </div>
 
 <?php require 'footer.php'; ?>
