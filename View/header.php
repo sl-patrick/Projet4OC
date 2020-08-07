734aa951b538aebf126535ec03b174a216daef3d
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-08-07 19:38:04 +0200

    contient le début du document HTML et l'ouverture de la balise body.

--- View/header.php
+++ View/header.php
@@ -2,27 +2,18 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./Public/css/style.css">
+    <script src="./Public/js/app.js" defer></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
     <title>Blog</title>
 </head>
 <body>
-    <header>
-        <div class="container">
-            <div class="row">
-                <nav class="col navbar navbar-expand navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Jean Forteroche</a>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a href="#" class="nav-link">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="View/chapterView.php" class="nav-link">Chapitres</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>    
-        </div>
-    </header>
+    <div class="container-fluid m-0 p-0">
+    
    
 
