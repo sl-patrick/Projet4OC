126db751c66970efcc04b3aa5c055a0d153f4ba7
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-08-11 11:17:35 +0200

    Contient le menu

---
+++ View/menu.php
@@ -0,0 +1,13 @@
+<header>
+    <nav class="col navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Jean Forteroche</a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="index.php" class="nav-link">Accueil</a>
+            </li>
+            <li class="nav-item">
+                <a href="index.php?url=chapter" class="nav-link">Chapitres</a>
+            </li>
+        </ul>
+    </nav>
+</header>
