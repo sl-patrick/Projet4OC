9614758e0e5c2b3f3697e4a1393a08feff453bdf
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-08-24 12:35:14 +0200

    Ajout des sections articles et commentaires récents / commentaires signalés

--- View/dashboard.php
+++ View/dashboard.php
@@ -7,6 +7,25 @@ require 'menu.php';
 ?>
 
 <div class="row m-0">
+    <!-- articles -->
+    <div class="col">
+        <h4>Articles récents</h4>
+    </div>
+    
+    <!-- commentaires -->
+    <div class="col">
+        <div class="row">
+            <div class="col">
+                <h4>Commentaires récents</h4>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <h4>Commentaires signaler</h4>
+            </div>
+        </div>
+    </div>
+
     
 </div>
 
