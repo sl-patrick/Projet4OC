304c13717431d687d6028791c1f23b15138bc5f9
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-08-24 12:37:49 +0200

    ajouter un article

---
+++ View/addPostView.php
@@ -0,0 +1,27 @@
+<?php
+
+$title = 'Ajouter un article';
+require 'header.php';
+require 'menu.php';
+
+?>
+
+<h3>Ajouter un article</h3>
+
+<div>
+    <form id="newPostForm" action="index.php?url=dashboard&amp;action=addPost" method="post">
+        <div class="form-group">
+            <label for="newTitle">Titre : </label>
+            <input type="text" name="newTitle" id="newTitle">
+        </div>
+        <div class="form-group">
+            <label for="newContents">Message :</label>
+            <textarea name="newContents" id="newContents" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="authorOfPost">Auteur : </label>
+            <input type="text" name="authorOfPost" id="authorOfPost">
+        </div>
+        <input type="submit" value="Ajouter" name="newPost">
+    </form>
+</div>
