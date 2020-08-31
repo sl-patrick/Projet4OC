4c6383e8f018a045b77cb04a63d4ac2fdad43c52
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-08-31 12:01:14 +0200

    méthode addCommentFromPost requêtes INSERT TO et SELECT

--- Model/Comment.php
+++ Model/Comment.php
@@ -4,6 +4,11 @@ require_once 'model.php';
 
 class Comment extends Database {
     
+    Public function getLastComments() {
+        $result = $this->getConnection()->query('SELECT id,pseudo,contents,creation_date FROM comments ORDER BY creation_date DESC LIMIT 0,5');
+        return $result;
+    }
+
     public function getCommentsFromPost($postId) {
         $result = $this->getConnection()->prepare('SELECT id,pseudo,contents,creation_date FROM comments WHERE id_post=?  ORDER BY creation_date DESC');
         $result->execute([$postId]);
@@ -11,13 +16,18 @@ class Comment extends Database {
     } 
 
     public function addCommentFromPost($postId, $contents, $author) {
-        $result = $this->getConnection()->prepare('INSERT INTO comments(pseudo, contents, id_post, creation_date) VALUES(:pseudo, :contents, :id_post, NOW())');
-        $result->execute(array(
+        $pdo = $this->getConnection();
+        $result = $pdo->prepare('INSERT INTO comments(pseudo, contents, id_post, creation_date) VALUES(:pseudo, :contents, :id_post, NOW())');
+        $result = $result->execute(array(
             'pseudo' => $author,
             'contents' => $contents,
             'id_post' => $postId
         ));
-        return $result;    
+        $commentId = $pdo->lastInsertId();
+        $comment = $pdo->prepare('SELECT id,pseudo,contents,creation_date FROM comments WHERE id= ?');
+        $comment->execute([$commentId]);
+        return $comment->fetch(PDO::FETCH_ASSOC);
+           
     }
 
     public function deleteComment($commentId) {
@@ -25,6 +35,13 @@ class Comment extends Database {
         $result->execute(array(
             'id' => $commentId
         ));
+    }
+
+    public function deleteCommentsFromPost($postId) {
+        $result = $this->getConnection()->prepare('DELETE FROM comments WHERE id_post = :id_post');
+        $result->execute(array(
+            'id_post' => $postId
+        )); 
     }
 
     public function reportComment($commentID) {}
