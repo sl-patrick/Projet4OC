f6fa71dc42fa147a8db63f5d132823b99d58d166
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-09-01 16:13:33 +0200

    Ajout du lien pour signaler un commentaire

--- View/displayCommentsView.php
+++ View/displayCommentsView.php
@@ -1,19 +1,18 @@
-<h2>Commentaires</h2>
-
 <?php
 
-while($comment = $comments->fetch()) {
-
-?>
-    <div>
+foreach ($comments as $comment) {
+    ?>
+    
+    <div class="displayComments">
         <p class="author"><?= htmlspecialchars($comment['pseudo']); ?></p>
         <p class="content"><?= htmlspecialchars($comment['contents']); ?></p>
         <p class="created_at"><?= htmlspecialchars($comment['creation_date']); ?></p>
+        <p class="text-right">
+            <a href="index.php?url=report&amp;idComment=<?= htmlspecialchars($comment['id']); ?>" class="reportComment">signaler</a>
+        </p>
     </div>
-<?php
 
+    <?php
 }
-
-$comments->closeCursor();
-
 ?>
+
