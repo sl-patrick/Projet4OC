80b50bca5671d5642718d2aea939e7eca5f05df2
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-09-10 13:10:13 +0200

    Ajout d'une div allComments

--- View/displayCommentsView.php
+++ View/displayCommentsView.php
@@ -1,18 +1,17 @@
-<?php
-
-foreach ($comments as $comment) {
+<div class="allComments">
+    <?php
+    foreach ($comments as $comment) {
     ?>
-    
-    <div class="displayComments">
-        <p class="author"><?= htmlspecialchars($comment['pseudo']); ?></p>
-        <p class="content"><?= htmlspecialchars($comment['contents']); ?></p>
-        <p class="created_at"><?= htmlspecialchars($comment['creation_date']); ?></p>
-        <p class="text-right">
-            <a href="index.php?url=report&amp;idComment=<?= htmlspecialchars($comment['id']); ?>" class="reportComment">signaler</a>
-        </p>
-    </div>
+        <div class="displayComments">
+            <p class="author"><?= htmlspecialchars($comment['pseudo']); ?></p>
+            <p class="content"><?= htmlspecialchars($comment['contents']); ?></p>
+            <p class="created_at"><?= htmlspecialchars($comment['creation_date']); ?></p>
+            <p class="text-right">
+                <a href="index.php?url=report&amp;idComment=<?= htmlspecialchars($comment['id']); ?>" class="reportComment">signaler</a>
+            </p>
+        </div>
 
     <?php
-}
-?>
-
+    }
+    ?>
+</div>
