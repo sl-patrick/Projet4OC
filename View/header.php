f2e0b4efb14d0630db45ab249fb3017a624d3e3e
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-09-14 16:32:36 +0200

    retrait des fichiers javascript

--- View/header.php
+++ View/header.php
@@ -5,13 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="./Public/css/style.css">
-    <script src="./Public/js/app.js" defer></script>
-    <script src="https://cdn.tiny.cloud/1/ad55exzrffm8mdl2lx2doscjfup83djqf8bey0coysmslqis/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script src="./Public/js/tiny.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-   
     <title><?= $title;?></title>
 </head>
 <body>
