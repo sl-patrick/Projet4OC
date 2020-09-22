7ffc96fca0424df2a675af1fbc4601ee5fe885f5
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-09-22 13:18:27 +0200

    Ajout onClick

--- View/instantCommentView.php
+++ View/instantCommentView.php
@@ -3,7 +3,7 @@
     <p class="content"><?= htmlspecialchars($comments['contents']); ?></p>
     <p class="created_at"><?= htmlspecialchars($comments['creation_date']); ?></p>
     <p class="text-right">
-        <a href="index.php?url=report&amp;idComment=<?= htmlspecialchars($comments['id']); ?>" class="reportComment">signaler</a>
+        <button type="button" class="btn btn-primary" data-url="index.php?url=report&amp;idComment=<?= htmlspecialchars($comments['id']); ?>">Signaler</button>
     </p>
 </div> 
 
