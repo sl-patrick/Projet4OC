02d4f52da524212171a9b5fc39ead13d861947d9
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-09-22 16:54:32 +0200

    méthode reportComment requête SELECT et UPDATE

--- Model/Comment.php
+++ Model/Comment.php
@@ -12,17 +12,17 @@ class Comment extends Database {
     
     public function addCommentFromPost($postId, $contents, $author) {
         $pdo = $this->getConnection();
-        $result = $pdo->prepare('INSERT INTO comments(pseudo, contents, id_post, creation_date) VALUES(:pseudo, :contents, :id_post, NOW())');
+        $result = $pdo->prepare('INSERT INTO comments(pseudo, contents, id_post, creation_date, report_comment) VALUES(:pseudo, :contents, :id_post, NOW(), :report_comment)');
         $result = $result->execute(array(
             'pseudo' => $author,
             'contents' => $contents,
-            'id_post' => $postId
+            'id_post' => $postId,
+            'report_comment' => 0
         ));
         $commentId = $pdo->lastInsertId();
         $comment = $pdo->prepare('SELECT id,pseudo,contents,creation_date FROM comments WHERE id= ?');
         $comment->execute([$commentId]);
-        return $comment->fetch(PDO::FETCH_ASSOC);
-        
+        return $comment->fetch(PDO::FETCH_ASSOC);   
     }
 
     Public function getLastComments() {
@@ -31,10 +31,24 @@ class Comment extends Database {
     }
 
     public function reportComment($commentId) {
-        $result = $this->getConnection()->prepare('INSERT INTO report_comments(id_comment,report_date) VALUES(:id_comment, NOW())');
-        $result->execute(array(
-            'id_comment' => $commentId
-        ));
+        $pdo = $this->getConnection();
+        $verify = $pdo->prepare('SELECT report_comment FROM comments WHERE id= ?');
+        $verify->execute([$commentId]);
+        $verify = $verify->fetch(PDO::FETCH_ASSOC);
+        
+        if (intval($verify['report_comment']) === 0) {
+            $result = $pdo->prepare('UPDATE comments SET report_comment = 1 WHERE id= ?');
+            $result->execute([$commentId]);
+            return intval($verify['report_comment']);
+        } else {
+            $result = $pdo->prepare('UPDATE comments SET report_comment = 0 WHERE id= ?');
+            $result->execute([$commentId]);
+            return intval($verify['report_comment']);
+        }
+    }
+
+    public function getLastReportComments() {
+        $result = $this->getConnection()->query('SELECT pseudo,contents,creation_date FROM comments WHERE report_comment = 1');
         return $result;
     }
 
