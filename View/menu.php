b4dbbd391074a517dc712707f66a645d99225ba7
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-01 17:53:27 +0200

    Adaptation du menu responsive.

--- View/menu.php
+++ View/menu.php
@@ -1,6 +1,10 @@
 <header>
-    <nav class="col navbar navbar-expand navbar-dark bg-dark">
+    <nav class="col navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="index.php">Jean Forteroche</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarResponsive" class="collapse navbar-collapse">
         <ul class="navbar-nav">
 <?php
 if (isset($_SESSION['pseudo'])) {
@@ -12,9 +16,12 @@ if (isset($_SESSION['pseudo'])) {
                 <a href="index.php?url=chapter" class="nav-link">Chapitres</a>
             </li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id="dashboardDropdown" aria-haspopup="true" aria-expanded="false">Tableau de bord</a>
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" id="dashboardDropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Tableau de bord</a>
                 <div class="dropdown-menu" aria-labelledby="dashboardDropdown">
-                    <a href="index.php?url=dashboard" class="dropdown-item">Accueil</a>
+                    <a href="index.php?url=dashboard&amp;action=posts" class="dropdown-item">Articles publiés</a>
+                    <a href="index.php?url=dashboard&amp;action=inWaiting" class="dropdown-item">Brouillons</a>
+                    <a href="index.php?url=dashboard&amp;action=comments" class="dropdown-item">Commentaires</a>
+                    <a href="index.php?url=dashboard&amp;action=reportComments" class="dropdown-item">Commentaires signalés</a>
                     <a href="index.php?url=dashboard&amp;action=addPost" class="dropdown-item">Ajouter un article</a>
                 </div>
             <li class="nav-item ">
@@ -34,5 +41,6 @@ if (isset($_SESSION['pseudo'])) {
 }
 ?>
         </ul>
+        </div>
     </nav>
 </header>
