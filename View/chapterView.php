6264c23d64cc1c2a65fd8f20a589795822d47c66
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-04 11:31:45 +0200

    Amélioration design et ajout système  pagination.

--- View/chapterView.php
+++ View/chapterView.php
@@ -12,24 +12,45 @@ require 'menu.php';
         </div>
     </section>
 
-    <div class="container">
-        <div class="row">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
         <?php
-            while ($post = $posts->fetch()) {
+            foreach ($posts as $post) {
             ?>
-                <div class="card">
+                <div class="card col-lg-5 m-2 p-0 text-center">
+                <div class="card-header"></div>
+
                     <div class="card-body">
-                        <h2 class="card-title"><a href="index.php?url=post&amp;postId=<?= htmlspecialchars($post['id']); ?>"><?= htmlspecialchars($post['title']); ?></a></h2>
-                        <p class="card-text"><?= htmlspecialchars($post['contents']); ?></p>
-                        <p><?= htmlspecialchars($post['author']); ?></p>
+                        <h2 class="card-title text-uppercase"><a class="card-link" href="index.php?url=post&amp;postId=<?= htmlspecialchars($post['id']); ?>"><?= htmlspecialchars($post['title']); ?></a></h2>
+                        <p class="card-text"><?= mb_strimwidth(htmlspecialchars($post['contents']), 0, 200, '...'); ?>
+                        <span>
+                            <a href="index.php?url=post&amp;postId=<?= htmlspecialchars($post['id']); ?>">Lire la suite</a>
+                        </span> 
+                        </p>
+                    </div>
+                    <div class="card-footer text-muted">
+                        <p>Par : <?= htmlspecialchars($post['author']); ?></p>
                         <p>Créé le : <?= htmlspecialchars($post['creation_date']); ?></p>
                     </div>
                 </div>
             <?php
             }
-            $posts->closeCursor();
             ?>
         </div>
+        <div class="page-pagination d-flex justify-content-center">
+    <nav>
+        <ul class="pagination">
+            <!-- previous -->
+            <li class="page-item mt-3 <?= ($currentPage === 1) ? 'disabled' : '' ?>">
+                <a class="page-link" href="index.php?url=chapter&amp;page=<?= $currentPage - 1; ?>">Précédente</a>
+            </li>
+            <!-- Next -->
+            <li class="page-item mt-3 <?= ($currentPage == $totalPage) ? 'disabled' : '' ?>">
+                <a class="page-link" href="index.php?url=chapter&amp;page=<?= $currentPage + 1; ?>">Suivante</a>
+            </li>
+        </ul>
+    </nav>
+</div>
     </div>
 </main>
 
