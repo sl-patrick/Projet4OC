a0ed9284a1a41515748dfcd83664b0f08c67e1cb
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-13 12:18:43 +0200

    Ajout d'un id à l'élément avec la classe displayComments et d'un data-id.

--- View/displayCommentsView.php
+++ View/displayCommentsView.php
@@ -1,14 +1,16 @@
-<div class="allComments">
+<div class="allComments mb-3">
     <?php
     foreach ($comments as $comment) {
     ?>
-        <div class="displayComments">
-            <p class="author"><?= htmlspecialchars($comment['pseudo']); ?></p>
-            <p class="content"><?= htmlspecialchars($comment['contents']); ?></p>
-            <p class="created_at"><?= htmlspecialchars($comment['creation_date']); ?></p>
-            <p class="text-right">
-                <button type="button" class="btn btn-primary" onclick="report(this.dataset.url)" data-url="index.php?url=report&amp;idComment=<?= htmlspecialchars($comment['id']); ?>">Signaler</button>
-            </p>
+        <div id="<?= htmlspecialchars($comment['id']); ?>" class="displayComments card shadow-sm mb-2">
+            <div class="card-body">
+                <h4 class="author card-title"><?= htmlspecialchars($comment['pseudo']); ?></h4>
+                <p class="content card-text"><?= htmlspecialchars($comment['contents']); ?></p>
+                <p class="created_at card-text text-muted"><?= htmlspecialchars($comment['creation_date']); ?></p>
+                <p class="text-right m-0">
+                    <button type="button" class="btn btn-dark" onclick="report(this.dataset.url, this.dataset.id)" data-id="<?= htmlspecialchars($comment['id']); ?>" data-url="index.php?url=report&amp;idComment=<?= htmlspecialchars($comment['id']); ?>">Signaler</button>
+                </p>
+            </div>
         </div>
 
     <?php
