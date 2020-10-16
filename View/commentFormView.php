7ec3d68698401b22d0d7811a126de35d7be897e2
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-16 16:58:08 +0200

    Refactorisation.

--- View/commentFormView.php
+++ View/commentFormView.php
@@ -1,16 +1,20 @@
-<h3>Ajouter un commentaire</h3>
+<div class="m-3">
+    <div class="col text-center">
+        <h3>Ajouter un commentaire</h3>
+    </div>
 
-<div>
     <form id="commentForm" action="index.php?url=postComment&amp;postId=<?= htmlspecialchars($_GET['postId']); ?>" method="post">
-        <div class="form-group">
-            <label for="author">Pseudo : </label>
-            <input type="text" name="author" id="author">
+        <div class="form-group row">
+            <small id="validationMessage" class="invalid-feedback text-center"></small>
         </div>
         <div class="form-group">
-            <label for="contents">Message : </label>
+            <label for="author">Pseudo</label>
+            <input type="text" name="author" id="author" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="contents">Message</label>
             <textarea name="contents" id="contents" class="form-control"></textarea>
         </div>
-        <input type="submit" value="Envoyer" class="send">
+        <input type="submit" value="Envoyer" class="send btn btn-dark">
     </form>
 </div>
-
