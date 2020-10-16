5b1018168dc9f50e3b66fa0c1ae04e1d4748d400
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-16 16:14:05 +0200

    AJAX message erreur formulaire commentaire.

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,17 +1,24 @@
 document.getElementById("commentForm").addEventListener("submit", function (event) {
 	event.preventDefault();
 	let dataForm = new FormData(this);
-	document.getElementById("author").value = null;
-	document.getElementById("contents").value = null;
 
 	const xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function () {
 		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 			let result = this.response;
-			let allComments = document.querySelector('.allComments');
-			let createDiv = document.createElement('div');
-			createDiv.innerHTML = result;
-			allComments.prepend(createDiv);	
+			if (result === "Tous les champs ne sont pas remplis") {
+				let message = document.getElementById("validationMessage");
+				message.innerHTML = result;
+				message.classList.add("d-block");
+			} else {
+				document.getElementById("author").value = null;
+				document.getElementById("contents").value = null;
+				document.getElementById("validationMessage").classList.remove("d-block");
+				let allComments = document.querySelector('.allComments');
+				let createDiv = document.createElement('div');
+				createDiv.innerHTML = result;
+				allComments.prepend(createDiv);
+			}
 		}
 	};
 	xhr.open("POST", this.action, true);
