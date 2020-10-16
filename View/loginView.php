0153d83bde62afc2f8d54dd3ccd55922921c3e30
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-16 17:02:07 +0200

    Refactorisation.

--- View/loginView.php
+++ View/loginView.php
@@ -1,31 +1,39 @@
-<?php 
+<?php
 
-$title = 'Connexion'; 
-require 'header.php'; 
+$title = 'Connexion';
+require 'header.php';
 ?>
 
 <main class="min-vh-100">
     <div class=" min-vh-100 container d-flex flex-column align-items-center justify-content-center text-center" id="login_view">
-    
+        <h1>Connexion</h1>
         <form id="loginForm" action="index.php?url=login&amp;action=connect" method="POST">
-            <div class="form-group">
+            <?php
+            if (isset($errorMessage)) {
+            ?>
+                <div class="form-group row">
+                    <small id="validationMessage" class="invalid-feedback d-block"><?= htmlspecialchars($errorMessage); ?></small>
+                </div>
+            <?php
+            }
+            ?>
+            <div class="form-group row">
                 <label for="pseudo" class="sr-only">Pseudo</label>
-                <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
+                <input class="form-control" type="text" name="pseudo" id="pseudo" placeholder="Pseudo" aria-describedby="validationMessage">
             </div>
-            <div class="form-group">
+            <div class="form-group row">
                 <label for="password" class="sr-only">Mot de passe</label>
-                <input type="password" name="password" id="password" placeholder="Mot de passe">
+                <input class="form-control" type="password" name="password" id="password" placeholder="Mot de passe" aria-describedby="validationMessage">
             </div>
-            <div class="form-group">
-                <input type="submit" name="submit" value="Connexion">
+            <div class="form-group row justify-content-center">
+                <input class="btn btn-dark" type="submit" name="submit" value="Connexion">
+            </div>
+            <div class="form-group row justify-content-center">
+                <a href="index.php">Retourner sur le site</a>
             </div>
         </form>
-        <a href="index.php">Retourner sur le site</a>
+
     </div>
 </main>
 
-</body>
-</html>
-
-
-
+<?php require 'footer.php'; ?>
