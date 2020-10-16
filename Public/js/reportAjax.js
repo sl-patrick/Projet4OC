96ff1857b1f42662cb4163112561448a1fa3d1bd
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-16 16:28:41 +0200

    refactorisation.

--- Public/js/reportAjax.js
+++ Public/js/reportAjax.js
@@ -2,22 +2,16 @@ function report(url, id) {
 	const xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function () {
 		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-			if (this.response === 0) {
-				console.log(id);
-				document.getElementById(id).classList.remove("bg-danger");
-				
-				console.log('commentaire non signaler');
-			} else if (this.response === 1) {
-				console.log('commentaire signaler');
+			if (this.response === 1) {
 				document.getElementById(id).classList.add("bg-danger");
 
-			} else {
-				console.log('un problème est survenu');
+			} else if (this.response === 0) {
+				document.getElementById(id).classList.remove("bg-danger");
+
 			}
 		}
 	};
 	xhr.open("POST", url, true);
 	xhr.responseType = "json";
 	xhr.send();
-	
 }
