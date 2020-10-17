f1ea9d6dea4266b644b159d107ded415ca2e8c06
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-18 00:19:57 +0200

    création const commentForm et d'une condition.

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,30 +1,34 @@
-document.getElementById("commentForm").addEventListener("submit", function (event) {
-	event.preventDefault();
-	let dataForm = new FormData(this);
-
-	const xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function () {
-		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-			let result = this.response;
-			if (result === "Tous les champs ne sont pas remplis") {
-				let message = document.getElementById("validationMessage");
-				message.innerHTML = result;
-				message.classList.add("d-block");
-			} else {
-				document.getElementById("author").value = null;
-				document.getElementById("contents").value = null;
-				document.getElementById("validationMessage").classList.remove("d-block");
-				let allComments = document.querySelector('.allComments');
-				let createDiv = document.createElement('div');
-				createDiv.innerHTML = result;
-				allComments.prepend(createDiv);
+const commentForm = document.getElementById("commentForm");
+if (commentForm) {
+	
+	commentForm.addEventListener("submit", function (event) {
+		event.preventDefault();
+		let dataForm = new FormData(this);
+	
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+				let result = this.response;
+				if (result === "Tous les champs ne sont pas remplis") {
+					let message = document.getElementById("validationMessage");
+					message.innerHTML = result;
+					message.classList.add("d-block");
+				} else {
+					document.getElementById("author").value = null;
+					document.getElementById("contents").value = null;
+					document.getElementById("validationMessage").classList.remove("d-block");
+					let allComments = document.querySelector('.allComments');
+					let createDiv = document.createElement('div');
+					createDiv.innerHTML = result;
+					allComments.prepend(createDiv);
+				}
 			}
-		}
-	};
-	xhr.open("POST", this.action, true);
-	xhr.responseType = "json";
-	xhr.send(dataForm);
-});
+		};
+		xhr.open("POST", this.action, true);
+		xhr.responseType = "json";
+		xhr.send(dataForm);
+	});
+}
 
 
 
