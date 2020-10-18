fb10ff07492673a6818685e2ce0bcc6b1bc9a7f6
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-18 13:40:11 +0200

    Ajout conditions dans les méthodes addPost, postWaiting, updatePost et refactorisation.

--- Controller/BackController.php
+++ Controller/BackController.php
@@ -1,89 +1,89 @@
 <?php
-
 require_once './Model/Post.php';
 require_once './Model/Comment.php';
 require_once './Model/User.php';
 
 
-class BackController
-{
+class BackController {
 
     private $_post;
     private $_comment;
     private $_user;
 
-    public function __construct()
-    {
+    public function __construct() {
         $this->_post = new Post();
         $this->_comment = new Comment();
         $this->_user = new User();
     }
 
-    public function addPostView()
-    {
+    public function addPostView() {
         require './View/addPostView.php';
     }
 
-    public function addPost($title, $contents, $author)
-    {
-        $verifyTitle = htmlspecialchars(strip_tags($title));
-        $verifyContents = htmlspecialchars($contents);
-
-        
-        $post = $this->_post->addPost($verifyTitle, $verifyContents, $author);
-        header('Location:./index.php?url=dashboard&action=posts');
+    public function addPost($title, $contents, $author) {
+        if (empty($title) OR empty($contents) OR empty($author)) {
+            $errorMessage = 'Tous les champs ne sont pas remplis';
+            require './View/addPostView.php';
+        } else {
+            $verifyTitle = htmlspecialchars(strip_tags($title));
+            $verifyContents = htmlspecialchars($contents);
+            $post = $this->_post->addPost($verifyTitle, $verifyContents, $author);
+            header('Location:./index.php?url=dashboard&action=posts');
+        }
     }
 
-    public function postWaiting($title, $contents, $author)
-    {
-        $verifyTitle = htmlspecialchars(strip_tags($title));
-        $verifyContents = htmlspecialchars($contents);
-
-        $post = $this->_post->inWaiting($verifyTitle, $verifyContents, $author);
-        header('Location:./index.php?url=dashboard&action=inWaiting');
+    public function postWaiting($title, $contents, $author) {
+        if (empty($title) OR empty($contents) OR empty($author)) {
+            $errorMessage = 'Tous les champs ne sont pas remplis';
+            require './View/addPostView.php';
+        } else {
+            $verifyTitle = htmlspecialchars(strip_tags($title));
+            $verifyContents = htmlspecialchars($contents);
+            $post = $this->_post->inWaiting($verifyTitle, $verifyContents, $author);
+            header('Location:./index.php?url=dashboard&action=inWaiting');
+        }
 
     }
 
-    public function getPost($postId)
-    {
+    public function getPost($postId) {
         $getArticle = $this->_post->getPost($postId);
         require './View/updatePost.php';
     }
 
-    public function updatePost($id, $title, $contents, $author)
-    {
-        
+    public function updatePost($id, $title, $contents, $author) {   
+        if (empty($title) OR empty($contents) OR empty($author)) {
+            header('Location:./index.php?url=dashboard&action=updatePost&postId='. $id .'');
+        } else {
         $verifyTitle = htmlspecialchars(strip_tags($title));
         $verifyContents = htmlspecialchars($contents);
         $updateArticle = $this->_post->updatePost($id, $verifyTitle, $verifyContents, $author);
         header('Location:./index.php?url=dashboard&action=inWaiting');
+        }
     }
 
     public function putInLine($id, $title, $contents, $author) {
-        $verifyTitle = htmlspecialchars(strip_tags($title));
-        $verifyContents = htmlspecialchars($contents);
-        $postPutInLine = $this->_post->putInLine($id, $verifyTitle, $verifyContents, $author);
-        header('Location:./index.php');
+        if (empty($title) OR empty($contents) OR empty($author)) {
+            header('Location:./index.php?url=dashboard&action=updatePost&postId='. $id .'');
+        } else {
+            $verifyTitle = htmlspecialchars(strip_tags($title));
+            $verifyContents = htmlspecialchars($contents);
+            $postPutInLine = $this->_post->putInLine($id, $verifyTitle, $verifyContents, $author);
+            header('Location:./index.php');
+        }
     }
 
-    public function deletePostWithComments($postId)
-    {
-
+    public function deletePostWithComments($postId) {
         $deletePost = $this->_post->deletePost($postId);
         $deleteComment = $this->_comment->deleteCommentsFromPost($postId);
         header('Location:./index.php?url=dashboard&action=posts');
-
     }
 
-    public function deleteComment($commentId)
-    {
+    public function deleteComment($commentId) {
         $deleteComment = $this->_comment->deleteComment($commentId);
         header('Location:./index.php?url=dashboard&action=reportComments');
-
     }
 
-    public function allPostsPagination($currentPage,$state)
-    {    
+    public function allPostsPagination($currentPage,$state) {    
         $countPosts = $this->_post->countPosts($state);
         $perPage = 5;
         $firstArticle = ($currentPage * $perPage) - $perPage;
@@ -101,9 +101,7 @@ class BackController
         }
     }
 
-    public function allCommentsPagination($currentPage,$state)
-    {
-
+    public function allCommentsPagination($currentPage,$state) {
         $countComments = $this->_comment->countAllComments($state);
         $perPage = 5;
         $firstComment = ($currentPage * $perPage) - $perPage;
@@ -119,11 +117,9 @@ class BackController
         } elseif ($state === 1) {
             require './View/reportComments.php';
         }
-
     }
    
-    public function connectUser($pseudo, $password)
-    {
+    public function connectUser($pseudo, $password) {
         if (empty($pseudo) OR empty($password)) {
             $errorMessage = 'Tous les champs ne sont pas remplis';
             require './View/loginView.php';
@@ -143,8 +139,7 @@ class BackController
         }
     }
 
-    public function logout()
-    {
+    public function logout() {
         session_destroy();
         header('Location:./index.php');
     }
