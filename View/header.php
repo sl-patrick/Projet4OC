db230d2f022b9baf79473da0a6d2eb973c918f50
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-18 14:35:39 +0200

    Ajout  balise meta e link.

--- View/header.php
+++ View/header.php
@@ -3,8 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta property="og:title" content="Jean Forteroche - Auteur et Écrivain" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Découvrez mon tout nouveau roman intitulé Billet simple pour l'Alaska" />
+    <meta property="og:image" content="" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="./Public/css/style.css">
+    <link rel="icon" type="image/png" href="./Images/favicon.png" />
+
     <title><?= $title;?></title>
 </head>
 <body>
