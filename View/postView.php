d0ab6d9045eac907a054b92bd64cf202ac880913
Patrick Samé Lhori <sl.patrick404@gmail.com>
2020-10-18 13:27:21 +0200

    Ajout d'un id à la section et refactorisation.

--- View/postView.php
+++ View/postView.php
@@ -1,49 +1,41 @@
 <?php
-
-$title = 'Mon blog';
-
+$title = 'Article';
 require 'header.php';
 require 'menu.php';
-
-
-foreach ($posts as $post) {
-
 ?>
 
-<main>
-    <section>
-        <div class="jumbotron jumbotron-fluid background text-center">
-            <div class="container">
-                <h2><?= htmlspecialchars($post['title']); ?></h2>
-                <p>par : <?= htmlspecialchars($post['author']); ?></p>
-                <p>Créé le : <?= htmlspecialchars($post['creation_date']); ?></p>
-            </div>
+<?php
+foreach ($posts as $post) {
+?>
+<section id="hero">
+    <div class="jumbotron jumbotron-fluid background text-center">
+        <div class="container-fluid background-opacity text-white">
+            <h2><?= htmlspecialchars($post['title']); ?></h2>
+            <p>par : <?= htmlspecialchars($post['author']); ?></p>
+            <p>Créé le : <?= htmlspecialchars($post['creation_date']); ?></p>
         </div>
-    </section>
+    </div>
+</section>
 
+<main class="min-vh-100">
     <div class="container">
         <div class="row m-3 justify-content-between">
             <a class="text-decoration-none" href="index.php">Retour à l'accueil</a>
             <a class="text-decoration-none" href="index.php?url=chapter">Chapitres</a>
-
         </div>
-        <!-- contenu article -->
         <div class="row">
             <div class="col text-center">
                 <p ><?= htmlspecialchars($post['contents']); ?></p>
             </div>
         </div>
         <hr class="my3">
-
     <?php
     }
-
     ?>
     
     <?php require 'displayCommentsView.php'; ?>
     <?php require 'commentFormView.php'; ?>
-
     </div>
 </main>
 
-    <?php require 'footer.php'; ?>
+<?php require 'footer.php'; ?>
